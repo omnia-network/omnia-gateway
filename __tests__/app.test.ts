461409fbf0093870eacd582c97bce5fecfb0b42c
Luca8991 <luca.berte98@hotmail.it>
2023-01-16 19:16:53 +0000

    initial commit

---
+++ __tests__/app.test.ts
@@ -0,0 +1,5 @@
+describe("Basic", () => {
+  it("should be 4", async () => {
+    expect(2+2).toEqual(4);
+  });
+});
---
+++ src/app.ts
@@ -0,0 +1,5 @@
+const main = async (): Promise<void> => {
+  console.log("Hello, world!");
+};
+
+main();
